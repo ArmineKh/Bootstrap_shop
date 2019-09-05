16fa12d52fd32b2c754cb7010cad1c61bcac2e87
Armine <https://github.com/ArmineKh>
2019-09-05 23:42:08 +0400

    added sidebar styles

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+	$('.carousel').carousel({
+		interval: false
+	});
+
+	$('.search').on('click', function(){
+		$('.main-menu .navbar-form').slideToggle();
+	});
+});
+
+// $(window).load(function(){
+// 	var carouselCaptionWidth = $('#carousel-sidebar .activ img').width();
+// 	$('#carousel-sidebar img').each(function(){
+// 		$(this).attr('width', carouselCaptionWidth);
+// 	});
+// 	$('#carousel-sidebar .sidebar-carousel-caption').css('max-width', carouselCaptionWidth + 'px');
+// });
